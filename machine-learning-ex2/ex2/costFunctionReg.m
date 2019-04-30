513909a132b986319e8d774ffca62f6ef9cfeda7
He <xiao.he@intel.com>
2019-04-30 14:40:34 +0800

    finish the assignment 2 logistic regression regularization

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -6,7 +6,10 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
+n = size(X,2);
+temp = ones(n,1);
+temp(1) = 0; %exclude theta(1), no need regularization for theta(1)
+A=eye(n);
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
@@ -17,8 +20,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
+hx = sigmoid(X*theta);
+J= (-y'*log(hx)-(1-y)'*log(1-hx))/m + lambda*(temp'*(theta.^2))/(2*m);
+grad = ((hx - y)'*X/m)' + lambda*A*theta/m;
+grad(1) = sum(hx-y)/m;
 
 
 
